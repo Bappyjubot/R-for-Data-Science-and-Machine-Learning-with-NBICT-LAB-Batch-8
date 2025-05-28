632643663ad8a8697330be5d4498097bae8d7c91
Bappyjubot <107bappy@gmail.com>
2025-05-28 22:52:59 +0600

    Create Scatter plot in R-(part_1).R

---
+++ RDSML_10 Scatter plot in R (part_1)/Scatter plot in R-(part_1).R
@@ -0,0 +1,17 @@
+# install.packages("ggplot2")
+library(ggplot2)
+
+dataset = mtcars
+
+# Generating a basic scatter plot
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point()
+
+# changing the point size and shape
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size=1, shape=16)
+
+# Adding the regression line
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size=1, shape=16) +
+  geom_smooth()
